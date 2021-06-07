0ea8babc4113d010a5ac4ac9ee49111bb1441cea
Jenp98 <82524216+Jenp98@users.noreply.github.com>
2021-06-07 17:39:29 +0200

    Multi cut test

--- Assignment 7/Prova/MainLoop.m
+++ Assignment 7/Prova/MainLoop.m
@@ -1,6 +1,6 @@
 clear;
 load data_farmer_cvar.mat;
-%yields = yields(1:50,:);
+yields = yields(1:50,:);
 S = size(yields,1);
 
 slopes=[];
@@ -17,45 +17,51 @@ lambda = 0.2;
 alpha = 0.05;
 Costs = [150 230 260]; %W;C;B
 j=0;
+newSlope = [];
+newValue = [];
+tildeF= [];
 t = [];
 Revenue = [];
+s=1;
+
 tic;
+i = 1;
 [tildeX, ~, optVal] = master(lambda, Costs, slopes, values, points);
 fprintf('First Master Solution\n')
 fprintf('tildeX %f\n', tildeX)
 fprintf('optVal: %f\n', optVal)
 while notConverged
-    t = [t, tildeX(4)];
-yalmip('clear'); 
-newSlope = 0; 
-newValue = 0; 
+yalmip('clear');  
 j = j+1;
 fprintf('\n\nLoop n: %f,\n\n', j)
-Revenues = 0;
-for s = 1:S
+newSlope = zeros(S,1,4);
+newValue = zeros(S,1);
+for s = 1:S 
 [value, mult, R] = recourse(lambda, yields, tildeX, s, alpha); 
-newSlope = newSlope + mult * 1/S;
-newValue = newValue + value * 1/S;
-Revenues = [Revenues, R];
+newSlope(s,1,:) = mult * 1/S;
+newValue(s,1) = value * 1/S;
 end
+slopes = [slopes, newSlope];
 fprintf('Solved Recourse problem\n')
 fprintf('newSlope: %f, \nnewValue: %f\n', newSlope, newValue);
-Revenue = [Revenue, mean(Revenues)];
+
 time = [time, toc]; 
 ub = [ub, optVal];
 lb = [lb, (optVal + tildeQ) - newValue]; 
 gap = [gap, newValue - tildeQ]; 
 fprintf('ub: %f, lb: %f, gap: %f, T: %f\n', ub(end), lb(end), gap(end),toc);
 
-if newValue - tildeQ < eps/1000
+if tildeQ == (1/S) * sum(newValue)
     %newValue == tildeQ
     %newValue - tildeQ < eps
     notConverged = false;
-  else
-    slopes = [slopes, newSlope];
-    values = [values, newValue];
+else
     points = [points, tildeX];
-    [tildeX, tildeQ, optVal] = master(lambda, Costs, slopes, values, points);
+    values =  [values, newValue(:,1)];
+    i = i +1;
+    [tildeX, theta, optVal] = master(lambda, Costs, slopes, values, points);
+    tildeQ = 1/S * sum(theta);
+    
     fprintf('Solved Master Problem:\n\n')
     fprintf('tildeX %f\n', tildeX)
     fprintf('tildeQ: %f, optVal: %f\n\n', tildeQ, optVal)
